ebc0d5dd476d98c7b79ec8356d09c318ea123a67
bilal Patiwala <bilalpatiwala04@gmail.com>
2022-11-04 23:51:24 +0530

    completed

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getDate = (note) => {
+  return new Date(note?.updated).toLocaleDateString()
+}
+
 let getTitle =  (note) => {
-  let title = note?.body?.split('\n')[0]
+  const title = note.body?.split('\n')[0]
   if(title?.lenght > 45){
     return title.slice(0, 45)
   }
-  return title
+  else{
+    return title
+  }
+  
 }
+
 
 const ListItems = ({ note }) => {
   return (
     <Link to={`/note/${note.id}`}>
       <div className="notes-list-item">
         <h2> { getTitle(note) } </h2>
+        <p> <span> {getDate(note)} </span> </p>
       </div>
     </Link>
   );
